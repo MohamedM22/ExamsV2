f40f80c01460ee963853ca9bfab7b6d2b498ef11
Mohamed Moulaye <109633435+MohamedM22@users.noreply.github.com>
2023-02-09 17:42:06 -0500

    Add files via upload

---
+++ Exams.java
@@ -0,0 +1,12 @@
+public class Exams {
+    public static void main(String[] args) {
+        String[] students = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+        // Instructions for this workbook are on Learn the Part (Workbook 6.2).
+        System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
+        for (int i = 0; i < students.length; i++){
+        System.out.println(students[i]+", you will take seat "+ i);
+        }
+        // Compare your result to what's on Learn the Part.
+
+    }
+}
